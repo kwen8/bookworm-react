c5e507a1a1aaa394f31025566e0250178b0781d9
chankwen <chenkwen6@gmail.com>
2018-07-30 13:18:36 +0800

    add login page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import HomePage from "../pages/HomePage";
+import LoginPage from "../pages/LoginPage";
 
-const App = () => <div>Hello world</div>;
+const App = () => (
+  <div>
+    <Route path="/" exact component={HomePage} />
+    <Route path="/login" component={LoginPage} />
+  </div>
+);
 
 export default App;
